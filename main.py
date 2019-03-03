1ec64d43b3b6fd16efd3691df1e6328106e0bcf3
Xavier Navarro <xanasa14@hotmail.com>
2019-03-03 16:49:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -47,3 +47,13 @@ taxi_modified[:,0] = 16
 Here, i replaced these 2 values 1800 and 1801 by the mean of the column 
 taxi_modified [1800:1802,7] = taxi_modified[:,7].mean()
 '''
+
+
+zeros = np.zeros([taxi_modified.shape[0], 1])
+taxi_modified = np.concatenate([taxi, zeros], axis=1)
+print(taxi_modified)
+#creating a boolean column 0 that becomes 1 if it is any of those 3 options. 
+taxi_modified [taxi_modified[:,5] == 2 ,15] = 1 
+taxi_modified [taxi_modified[:,5] == 3, 15] = 1
+taxi_modified [taxi_modified[:,5] == 5, 15] = 1
+
